d9c44eeb83d06ce0551021eb4bfa34b073a14772
bumzack <bumzacktrara@gmail.com>
2019-09-01 22:53:08 +0200

    added cuda feature to crate "raytracer"

--- raytracer/src/backend/mod.rs
+++ raytracer/src/backend/mod.rs
@@ -3,7 +3,8 @@ pub use self::backend_cpu::*;
 
 mod backend;
 mod backend_cpu;
-// pub mod backend_cuda;
 
 pub const MAX_REFLECTION_RECURSION_DEPTH: i32 = 10;
 
+#[cfg(feature = "cuda")]
+pub mod backend_cuda;
--- raytracer/src/lib.rs
+++ raytracer/src/lib.rs
@@ -2,8 +2,17 @@
 
 extern crate cpu_kernel_raytracer;
 extern crate raytracer_lib_std;
+#[cfg(feature = "cuda")]
+extern crate rustacuda;
+#[cfg(feature = "cuda")]
+extern crate rustacuda_core;
+#[cfg(feature = "cuda")]
+#[macro_use]
+extern crate rustacuda_derive;
 
 pub use self::backend::*;
+#[cfg(feature = "cuda")]
+pub use self::backend_cuda::*;
 pub use self::cpu_kernel_raytracer::*;
 pub use self::raytracer_lib_std::*;
 
