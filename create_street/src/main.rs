42e57b2695e5eb4b49c8d40715e6486694a61757
bumzack <bumzacktrara@gmail.com>
2019-09-10 20:37:18 +0200

    debug and real output in same method

--- create_street/src/main.rs
+++ create_street/src/main.rs
@@ -8,17 +8,42 @@ fn main() {
 }
 
 fn main_coord_axes() {
-    let width = 640;
-    let height = 480;
+    let width = 160;
+    let height = 100;
+
+    let debug = true;
 
     let (mut world, mut camera) = coord_axes::setup_world_coord_axes(width, height, false);
     coord_axes::add_floor(&mut world);
     coord_axes::add_borders(&mut world);
 
     let backend = BackendCuda::new();
-    // let backend = BackendCpu::new();
+    let backend = BackendCpu::new();
 
-    let (frames, delta) = (10, 0.8);
+    let x = 82;
+    let y = 10;
+
+
+    let mut camera_from = Tuple4D::new_point(2.0, 2.0, -2.0);
+    let mut camera_to = Tuple4D::new_point(0.0, 0.0, 0.0);
+    let mut camera_up = Tuple4D::new_vector(0.0, 1.0, 0.0);
+
+    let mut light_pos = Tuple4D::new_point(2.0, 7.0, -2.0);
+
+    println!("light pos = {:?}", light_pos);
+
+    let pl = PointLight::new(light_pos, Color::new(1.0, 1.0, 1.0));
+    let l = Light::PointLight(pl);
+    world.set_light(l);
+    camera.set_transformation(Matrix::view_transform(&camera_from, &camera_to, &camera_up));
+    let canvas = backend.render_world_debug(&mut world, &camera, x, y);
+    let filename = format!("./create_street/img/debug_point_{}_{}.png", x, y);
+
+
+    // real deal
+
+
+    let (frames, delta) = (1, 0.8);
     //    let (frames, delta) = (25, 0.6);
 
     let is_3d = true;
@@ -48,7 +73,7 @@ fn main_wall_with_2_tilsts() {
 
     let backend = BackendCuda::new();
 
-    let (frames, delta) = (420, 0.02);
+    let (frames, delta) = (1, 0.02);
     //    let (frames, delta) = (25, 0.6);
 
     let is_3d = false;
