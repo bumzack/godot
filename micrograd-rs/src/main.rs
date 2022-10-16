6419a6f4deb44c37cb0663b5119489b7892e7e4a
bumzacktrara <bumzacktrara@gmail.com>
2022-10-17 00:16:41 +0200

    add and mul working, no grad tough

--- micrograd-rs/src/main.rs
+++ micrograd-rs/src/main.rs
@@ -1,24 +1,24 @@
 use std::cell::{Ref, RefCell};
 use std::fmt::{Display, Formatter};
-use std::ops::Add;
+use std::ops::{Add, Mul};
 use std::rc::Rc;
 
 fn main() {
-    let a = ValueRef::new_value(2.0);
+    let a  = ValueRef::new_value(2.0 as f64);
     let b = ValueRef::new_value(3.0);
-
     let c = ValueRef::add(&a, &b);
-
-    //  println!("c = {}", c.r.borrow().data);
     let d = ValueRef::new(Value::new_value(8.0));
+    let e = ValueRef::mul(&c, &d);
 
-    // let e = c * d;
-
-    // println!("a = {:?}", &*a.r.borrow());
-    // println!("b = {:?}", &*b.r.borrow());
-    //  println!("c = {:?}", &*c.r.borrow());
-    // println!("d = {}", &*d.0.borrow());
-    // println!("e = {}", &*e.0.borrow());
+    let x = &a+&b;
+    let y = &a*&x;
+    println!("a = {:?}", a.borrow().data);
+    println!("b = {:?}", b.borrow().data);
+    println!("c = {:?} = a+b", c.borrow().data);
+    println!("d ={:?}", d.borrow().data);
+    println!("e = {:?} = c*d", e.borrow().data);
+    println!("x = {:?} = a+b", x.borrow().data);
+    println!("y = {:?} = a*x", y.borrow().data);
 
     println!("DONE");
 }
@@ -27,47 +27,66 @@ fn main() {
 enum OpEnum {
     NONE,
     ADD,
-    //   MUL,
+    MUL,
 }
 
 #[derive(Debug, Clone)]
-struct Value {
-    data: f64,
+struct Value<T> where T: Clone + Add + Mul {
+    data: T,
     op: OpEnum,
     //  grad: T,
-    inp1: Option<ValueRef>,
-    inp2: Option<ValueRef>,
+    inp1: Option<ValueRef<T>>,
+    inp2: Option<ValueRef<T>>,
 }
 
 #[derive(Debug, Clone)]
-struct ValueRef {
-    r: Rc<RefCell<Value>>,
+struct ValueRef<T> where T: Clone + Add + Mul {
+    r: Rc<RefCell<Value<T>>>,
 }
 
-impl ValueRef {
-    fn new(v: Value) -> Self {
+impl<T> ValueRef<T> where T: Clone + Add<Output=T> + Mul<Output=T> {
+    fn new(v: Value<T>) -> Self {
         ValueRef {
             r: Rc::new(RefCell::new(v)),
         }
     }
 
-    fn new_value(v: f64) -> ValueRef {
+    fn new_value(v: T) -> ValueRef<T> {
         let v = Value::new_value(v);
         ValueRef {
             r: Rc::new(RefCell::new(v)),
         }
     }
 
-    fn borrow(&self) -> Ref<Value> {
+    fn borrow(&self) -> Ref<Value<T>> {
         self.r.borrow()
     }
 
-    fn add(a: &ValueRef, b: &ValueRef) -> Self {
-        let x1 = a.borrow();
-        let x2 = b.borrow();
+    fn add(a: &ValueRef<T>, b: &ValueRef<T>) -> Self
+        where <T as Add>::Output: Clone, <T as Add>::Output: Add,
+              <T as Mul>::Output: Clone, <T as Mul>::Output: Mul
+    {
+        let x1 = a.borrow().clone();
+        let x2 = b.borrow().clone();
         let v = Value {
-            data: &x1.data + &x2.data,
+            data: x1.data + x2.data,
             op: OpEnum::ADD,
+            //   grad: 1.0,
+            inp1: Some(a.clone()),
+            inp2: Some(b.clone()),
+        };
+        ValueRef::new(v)
+    }
+
+    fn mul(a: &ValueRef<T>, b: &ValueRef<T>) -> Self
+        where <T as Add>::Output: Clone, <T as Add>::Output: Add,
+              <T as Mul>::Output: Clone, <T as Mul>::Output: Mul
+    {
+        let x1 = a.borrow().clone();
+        let x2 = b.borrow().clone();
+        let v = Value {
+            data: x1.data * x2.data,
+            op: OpEnum::MUL,
             //   grad: 1.0,
             inp1: Some(a.clone()),
             inp2: Some(b.clone()),
@@ -76,9 +95,9 @@ impl ValueRef {
     }
 }
 
-impl Value
+impl<T> Value<T> where T: Clone + Add + Mul
 {
-    fn new(data: f64, op: OpEnum) -> Self {
+    fn new(data: T, op: OpEnum) -> Self {
         Value {
             data,
             op,
@@ -88,7 +107,7 @@ impl Value
         }
     }
 
-    fn new_value(data: f64) -> Self {
+    fn new_value(data: T) -> Self {
         Value {
             data,
             op: OpEnum::NONE,
@@ -98,27 +117,69 @@ impl Value
         }
     }
 }
+
 //
-// impl<'a, T> Add<ValueRef<T>> for ValueRef<T>
-//     where
-//         T: Add + Default + Display+'a,
-//         &'a T: Add<&'a T>,
-// {
-//     type Output = ValueRef<T>;
-//
-//     fn add(self, rhs: ValueRef<T>) -> Self::Output {
-//         let x1 = &self.r.clone().borrow().data;
-//         let x2 = &rhs.r.clone().borrow().data;
-//         let v = Value {
-//             data: x1 + x2,
-//             op: OpEnum::ADD,
-//             grad: T::default(),
-//             inp1: None,
-//             inp2: None,
-//         };
-//         ValueRef::new(v)
-//     }
-// }
+impl<'a, 'b, T> Add<&'b ValueRef<T>> for &'a ValueRef<T>
+    where T: Clone + Add + Mul + Add<Output=T> + Mul<Output=T>,
+          <T as Add>::Output: Clone,
+          <T as Mul>::Output: Clone,
+          <T as Add>::Output: Add,
+          <T as Mul>::Output: Mul,
+          <T as Add>::Output: Mul
+
+{
+    type Output = ValueRef<T>;
+
+    fn add(self, rhs: &'b ValueRef<T>) -> Self::Output {
+        let x1 = self.r.borrow().clone();
+        let x2 = rhs.r.borrow().clone();
+        let v = Value {
+            data: x1.data + x2.data,
+            op: OpEnum::ADD,
+            //   grad: 1.0,
+            inp1: Some(self.clone()),
+            inp2: Some(rhs.clone()),
+        };
+        ValueRef::new(v)
+    }
+
+    // fn add(self, rhs: &'b ValueRef<T>) -> ValueRef<T> {
+    //     let v = ValueRef::<T>::blupp(self, rhs, OpEnum::ADD);
+    //     ValueRef::new(v)
+    //}
+}
+
+impl<'a, 'b, T> Mul<&'b ValueRef<T>> for &'a ValueRef<T>
+    where T: Clone + Add + Mul + Add<Output=T> + Mul<Output=T>,
+          <T as Add>::Output: Clone,
+          <T as Mul>::Output: Clone,
+          <T as Add>::Output: Add,
+          <T as Mul>::Output: Mul,
+          <T as Add>::Output: Mul
+
+{
+    type Output = ValueRef<T>;
+
+    fn mul(self, rhs: &'b ValueRef<T>) -> Self::Output {
+        let x1 = self.r.borrow().clone();
+        let x2 = rhs.r.borrow().clone();
+        let v = Value {
+            data: x1.data * x2.data,
+            op: OpEnum::ADD,
+            //   grad: 1.0,
+            inp1: Some(self.clone()),
+            inp2: Some(rhs.clone()),
+        };
+        ValueRef::new(v)
+    }
+
+    // fn add(self, rhs: &'b ValueRef<T>) -> ValueRef<T> {
+    //     let v = ValueRef::<T>::blupp(self, rhs, OpEnum::ADD);
+    //     ValueRef::new(v)
+    //}
+}
+
+
 //
 // impl<T: Add + Default + Display> Add<T> for Value<T> {
 //     type Output = Value<T>;
@@ -199,7 +260,7 @@ impl Display for OpEnum {
         match self {
             OpEnum::ADD => write!(f, "ADD"),
             OpEnum::NONE => write!(f, "NONE"),
-            //       OpEnum::MUL => write!(f, "MUL"),
+            OpEnum::MUL => write!(f, "MUL"),
         }
     }
 }
