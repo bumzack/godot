59383b53ff37ec4f7ebb1d55363bff3a93d69991
bumzacktrara <bumzacktrara@gmail.com>
2022-10-23 11:49:06 +0200

    fmt

--- micrograd-rs/src/graph_v2.rs
+++ micrograd-rs/src/graph_v2.rs
@@ -20,8 +20,6 @@ pub fn draw_graph(root: ValueRefV2, filename: String) {
 
 
 
-
-
                                         Dot::with_config(&graph, &[Config::EdgeNoLabel]))).unwrap();
     let graph_svg = exec(
         graphviz_graph,
