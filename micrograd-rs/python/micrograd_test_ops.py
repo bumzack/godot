4ce5b35395997a883c08eecbc61ed2a73b9f589e
bumzacktrara <bumzacktrara@gmail.com>
2022-10-30 23:14:44 +0100

    python tests added local copy of python micorgrad and added methods from the yt videos

---
+++ micrograd-rs/python/micrograd_test_ops.py
@@ -0,0 +1,165 @@
+from mgrad.engine import Value
+
+a = Value(-5)
+b = a.relu()
+a.backward()
+b.grad = 3
+print(f"a  {a},  relu(a) {b}")
+
+a = Value(0)
+b = a.relu()
+b.grad = 2
+a.backward()
+print(f"a  {a},  relu(a) {b}")
+
+a = Value(23)
+b = a.relu()
+b.grad = 4
+a.backward()
+print(f"a  {a},  relu(a) {b}")
+
+print("#########################################")
+
+a = Value(23)
+b = Value(45)
+c = a + b
+print(f"c = a + b = {a} + {b}  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"b.grad:  {b.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+print("#########################################")
+
+a = Value(23)
+b = Value(45)
+c = a - b
+print(f"c = a - b = {a} - {b}  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"b.grad:  {b.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(12)
+b = Value(23)
+c = a * b
+print(f"c = a * b = {a} *  {b}  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"b.grad:  {b.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(7)
+b = Value(2)
+c = a / b
+print(f"c = a / b = {a}  / {b}  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"b.grad:  {b.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(7)
+c = a.relu()
+print(f"c = a.relu = {a}.relu() = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(0)
+c = a.relu()
+print(f"c = a.relu = {a}.relu() = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(-1)
+c = a.relu()
+print(f"c = a.relu = {a}.relu() = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(-1)
+b = 2
+c = a ** b
+print(f"c = a ** b = {a} ** {b}  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(3)
+b = 3
+c = a ** b
+print(f"c = a ** b = {a} ** {b}  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(3)
+b = 1.5
+c = a ** b
+print(f"c = a ** b = {a} ** {b}  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+# def tanh(self):
+#     x = self.data
+#     t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+#     out = Value(t, (self,), 'tanh')
+#
+#     def _backward():
+#         self.grad += (1 - t ** 2) * out.grad
+#
+#     out._backward = _backward
+#
+#     return out
+
+
+a = Value(2)
+c = a.tanh()
+print(f"c = a.tanh() = {a}.tanh()  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(-1)
+c = a.tanh()
+print(f"c = a.tanh() = {a}.tanh()  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
+a = Value(0)
+c = a.tanh()
+print(f"c = a.tanh() = {a}.tanh()  = {c}")
+
+c.backward()
+print(f"a.grad:  {a.grad}  ")
+print(f"c.grad:  {c.grad}  ")
+print("#########################################")
+
