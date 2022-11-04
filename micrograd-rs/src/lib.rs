263e4192bc77fe34afa9e524bdbc8bdf90e6b28e
bumzacktrara <bumzacktrara@gmail.com>
2022-11-04 22:45:25 +0100

    tensors: first tests pass

--- micrograd-rs/src/lib.rs
+++ micrograd-rs/src/lib.rs
@@ -7,7 +7,23 @@ pub mod micrograd_rs_engine_v3;
 pub mod micrograd_rs_engine_v4_tensor;
 pub mod micrograd_rs_v2;
 pub mod micrograd_rs_v3;
+pub mod micrograd_rs_v4_mathtensor;
 pub mod micrograd_rs_v4_tensor;
+pub mod micrograd_rs_v4_tensorinternal;
+
+pub const EPS: f64 = 0.0001;
+pub const EPS2: f64 = 0.3;
+
+pub fn assert_two_float(a: f64, b: f64) -> bool {
+    if (a - b).abs() < EPS {
+        return true;
+    }
+    false
+}
+
+pub fn assert_float(actual: f64, expected: f64) {
+    assert!(assert_two_float(actual, expected));
+}
 
 // pub mod prelude {
 //     pub use super::graph_v2::*;
