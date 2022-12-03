fb98815b906aad177ebc335b72d3da0786518c89
bumzacktrara <bumzacktrara@gmail.com>
2022-12-03 23:08:26 +0100

    WIP 3D renderer - lazy for Piston window is bad, now the fps from the piston game loop work

--- renderer_benny/examples/test_piston.rs
+++ renderer_benny/examples/test_piston.rs
@@ -2,168 +2,153 @@ extern crate piston_window;
 
 use std::f32::consts::PI;
 use std::path::PathBuf;
-use std::time::Instant;
+use std::process::exit;
+use std::thread;
+use std::time::{Duration, Instant};
 
-use image::ImageBuffer;
-use piston_window::{
-    clear, text, Button, Context, G2d, G2dTexture, Glyphs, Key, PistonWindow, PressEvent, Window, WindowSettings,
-};
-use pretty_env_logger::env_logger::fmt::Color;
-use rand::thread_rng;
+use piston_window::{Button, clear, Key, PistonWindow, PressEvent, text, TextureContext, WindowSettings};
 
-use render_benny::prelude::{
-    Camera, Canvas, CanvasOps, CanvasOpsStd, Matrix, MatrixOps, Mesh, Pixel, Quaternion, RenderContext, Transform,
-    Tuple, Tuple4D,
-};
+use render_benny::prelude::{Game, MonkeyDisplay};
 
 use crate::piston_window::EventLoop;
 use crate::piston_window::Transformed;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut window: PistonWindow = WindowSettings::new("Godot: Renderer", [1280, 720])
+    let win_width = 1600;
+    let win_height = 900;
+    let opengl = piston_window::OpenGL::V3_2;
+    let mut window: piston_window::PistonWindow = piston_window::WindowSettings::new("piston: image", [win_width, win_height])
         .exit_on_esc(true)
-        //.opengl(OpenGL::V2_1) // Set a different OpenGl version
+        .graphics_api(opengl)
         .build()
         .unwrap();
 
-    // let assets = find_folder::Search::ParentsThenKids(3, 3)
-    //     .for_folder("assets").unwrap();
+    //
+    // let texture_context = TextureContext {
+    //     factory: window.factory.clone(),
+    //     encoder: window.factory.create_command_buffer().into(),
+    // };
 
-    let (assets, width, height, texture2, mut monkey_mesh, mut monkey_transform, mut camera) = init();
+    // let (sender, recv_buffer) = unbounded::<Vec<u8>>();
+    // let (sender_delta, recv_delta) = unbounded::<f32>();
+
+    let mut game = MonkeyDisplay::init();
+    game.start();
+
+    let assets = PathBuf::from("/Users/bumzack/stoff/rust/godot/renderer_benny/res/");
+    println!("{:?}", assets);
 
     let mut glyphs = window.load_font(assets.join("FiraSans-Regular.ttf")).unwrap();
-    let mut window = window.max_fps(60);
+    let mut window = window.max_fps(120);
 
     let mut previous = Instant::now();
-    window.set_lazy(true);
+    // window.set_lazy(true);
 
     let mut frame = 0;
+    let mut delta = 0.0;
+    let mut frame_duration = Duration::from_micros(0);
+    let texture_context = &mut window.create_texture_context();
+
+    let width = game.width;
+    let height = game.height;
+
+    let t = game.update(delta, texture_context);
 
     while let Some(e) = window.next() {
+        let start = Instant::now();
         if let Some(Button::Keyboard(Key::S)) = e.press_args() {
             println!("S pressed");
         }
 
-        // camera.update(delta.as_secs_f32());
-        camera.update(frame as f32);
-        let vp = camera.get_view_projection();
+        // crossbeam::scope(|s| {}).expect("crossbeam crash");
+        //
+        //
+        // let texture = crossbeam::scope(|s| {
+        //     let mut children = vec![];
+        //
+        //     for _i in 0..1 {
+        //         children.push(s.spawn(move |_| {
+        //             println!("     thread_id {:?}", thread::current().id());
+        //
+        //             let a = game.update(delta, texture_context);
+        //
+        //             (thread::current().id(), a)
+        //         }));
+        //     }
+        //
+        //     let mut res = vec![];
+        //     for child in children {
+        //         let dur = Instant::now() - start;
+        //         let (thread_id, texture) = child.join().unwrap();
+        //         res.push(texture);
+        //         println!(
+        //             "child thread {:?} finished. run for {:?}  ",
+        //             thread_id, dur
+        //         );
+        //     }
+        //     let dur = Instant::now() - start;
+        //     Ok(res[0])
+        // }).expect("thread ended with error");
 
-        let start = Instant::now();
-        monkey_transform = monkey_transform.rotate(Quaternion::new_from_tuple_and_angle(
-            Tuple4D::new_vector(0.0, 1.0, 0.0),
-            // delta.as_secs_f32(),
-            frame as f32,
-        ));
+
+        let t = game.update(delta, texture_context);
+
+        // match recv_buffer.recv() {
+        //     Ok(buffer) => {
         let dur = Instant::now() - start;
-        println!("duration rotate {:?}", dur);
+        println!("game.update frame {} {:?}", frame, dur);
         let start = Instant::now();
 
-        let mut target = RenderContext::new(width, height);
+        //  let img = ImageBuffer::from_raw(width as u32, height as u32, buffer).unwrap();
 
-        monkey_mesh.draw(&mut target, &vp, &monkey_transform.get_transformation(), &texture2);
-
-        let dur = Instant::now() - start;
-        println!("draw mesh rotation {:?}", dur);
-        let start = Instant::now();
-
-        let mut buffer: Vec<u8> = vec![0; width * height * 4];
-
-        let pixels = target.canvas().get_pixels();
-        for (i, p) in target.canvas().get_pixels().iter().enumerate() {
-            let idx = i * 4;
-            let c = p.color;
-            buffer[idx] = (c.r * 255.0) as u8;
-            buffer[idx + 1] = (c.g * 255.0) as u8;
-            buffer[idx + 2] = (c.b * 255.0) as u8;
-            buffer[idx + 3] = 255;
-        }
-
-        target.canvas().get_pixels().iter().for_each(|p| {});
-
-        let dur = Instant::now() - start;
-        println!("copy canvas to u8 Vec buffer {:?}", dur);
-        let start = Instant::now();
-
-        let img = ImageBuffer::from_raw(
-            target.canvas().get_width() as u32,
-            target.canvas().get_height() as u32,
-            buffer,
-        )
-        .unwrap();
-
-        let dur = Instant::now() - start;
-        println!("creating piston image buffer {:?}", dur);
-        let start = Instant::now();
-
-        let texture: piston_window::G2dTexture = piston_window::Texture::from_image(
-            &mut window.create_texture_context(),
-            &img,
-            &piston_window::TextureSettings::new(),
-        )
-        .unwrap();
-
-        let dur = Instant::now() - start;
-        println!("creating piston image G2dTexture {:?}", dur);
-        let start = Instant::now();
+        // let t: piston_window::G2dTexture = piston_window::Texture::from_image(
+        //     texture_context,
+        //     &img,
+        //     &piston_window::TextureSettings::new(),
+        // )
+        //.unwrap();
 
         frame += 1;
+        delta += 0.2;
 
         window.draw_2d(&e, |c, g, device| {
-            let transform = c.transform.trans(10.0, 700.0);
+            let transform_text = c.transform.trans(10.0, (win_height-30)as f64);
 
             clear([0.0, 0.0, 0.0, 1.0], g);
-            // let dur = Instant::now() - start;
-            // println!("clear window {:?}", dur);
-            // let start = Instant::now();
-            //
-            // text::Text::new_color([0.0, 1.0, 0.0, 1.0], 32)
-            //     .draw("Hello Godot!", &mut glyphs, &c.draw_state, transform, g)
-            //     .unwrap();
-            //
-            // // Update glyphs before rendering.
-            // glyphs.factory.encoder.flush(device);
+            let dur = Instant::now() - start;
+            println!("frame {} clear window {:?}", frame, dur);
+            let start = Instant::now();
+
+            let s = format!("frame {}, dur {:?}, Hello Godot! ", frame, frame_duration);
+            text::Text::new_color([0.0, 1.0, 1.0, 1.0], 32)
+                .draw(&s, &mut glyphs, &c.draw_state, transform_text, g)
+                .unwrap();
+
+            // Update glyphs before rendering.
+            glyphs.factory.encoder.flush(device);
 
             let img_transform = c.transform.trans(0.0, 0.0);
 
-            piston_window::image(&texture, img_transform, g);
+            piston_window::image(&t, img_transform, g);
             let dur = Instant::now() - start;
             println!("drawing image in piston window {:?}", dur);
         });
+        //     }
+        //     Err(e) => panic!("receiving buffer error {}", e),
+        // }
 
         let new = Instant::now();
-        let duration = new - previous;
+        frame_duration = new - previous;
         println!(
             "frame {},  got an event.  duration {} ms      ({} us)",
             frame,
-            duration.as_millis(),
-            duration.as_micros()
+            frame_duration.as_millis(),
+            frame_duration.as_micros()
         );
         previous = new;
+        // if duration.as_millis() > 20 && frame>1 {
+        //     exit(1);
+        // }
     }
     Ok(())
-}
-
-fn init() -> (PathBuf, usize, usize, Canvas, Mesh, Transform, Camera) {
-    let assets = PathBuf::from("/Users/bumzack/stoff/rust/godot/renderer_benny/res/");
-    println!("{:?}", assets);
-
-    let width = 800;
-    let height = 600;
-
-    let texture2 = Canvas::read_bitmap("/Users/bumzack/stoff/rust/godot/renderer_benny/res/bricks2.jpg")
-        .expect("could not find asset file");
-    let monkey_mesh = Mesh::read_obj_file("/Users/bumzack/stoff/rust/godot/renderer_benny/res/smoothMonkey0.obj")
-        .expect("could not find asset file");
-
-    let mut monkey_transform = Transform::new_from_vector(Tuple4D::new_point(0.0, 0.0, 2.0));
-    //    let terrain_transform = Transform::new_from_vector(Tuple4D::new_vector(0.0, -1.0, 0.0));
-    println!("monkey_transform original = {:?}", monkey_transform);
-
-    let fov = 70.0 * PI / 180.0;
-    let aspect_ratio = width as f32 / height as f32;
-    let z_near = 0.1;
-    let z_far = 10.0;
-    let m = Matrix::init_perspective(fov, aspect_ratio, z_near, z_far);
-    let mut camera = Camera::new(m);
-    (assets, width, height, texture2, monkey_mesh, monkey_transform, camera)
 }
