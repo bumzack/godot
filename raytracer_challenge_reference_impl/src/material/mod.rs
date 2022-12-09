7873a3c3200dc0eb3280aa920eb066e3efa4cf97
bumzack <bumzacktrara@gmail.com>
2022-12-09 14:27:17 +0100

    fix develop (#18)

---
+++ raytracer_challenge_reference_impl/src/material/mod.rs
@@ -0,0 +1,3 @@
+pub use self::material::*;
+
+pub mod material;
---
+++ raytracer_challenge_reference_impl/src/math/mod.rs
@@ -0,0 +1,7 @@
+pub mod common;
+pub mod matrix;
+pub mod tuple4d;
+
+pub use self::common::*;
+pub use self::matrix::*;
+pub use self::tuple4d::*;
